e38559a39ad5355473898ca2f164d8ed46d51d79
aditya nur atha <Praktikum_APD_B2-25>
2026-05-05 22:05:07 +0800

    mengedit menu admin

--- admin.cpp
+++ admin.cpp
@@ -17,48 +17,46 @@ void tampilkanHero() {
     json data;
     file >> data;
 
-    cout << "\n===== DAFTAR HERO =====\n";
+    cout << "\n===== PENGELOLAAN SUPERHERO =====\n";
 
-for (auto &hero : data["heroes"]) {
-    cout << "Nama     : " << hero["name"] << endl;
-    cout << "Points   : " << hero["points"] << endl;
+    for (auto &hero : data["heroes"]) {
+        cout << "Nama     : " << hero["name"] << endl;
+        cout << "Points   : " << hero["points"] << endl;
 
-    cout << "Combat   : " << hero["stats"]["combat"] << endl;
-    cout << "Vigor    : " << hero["stats"]["vigor"] << endl;
-    cout << "Mobility : " << hero["stats"]["mobility"] << endl;
-    cout << "Charisma : " << hero["stats"]["charisma"] << endl;
-    cout << "Intellect: " << hero["stats"]["intellect"] << endl;
+        cout << "Combat   : " << hero["stats"]["combat"] << endl;
+        cout << "Vigor    : " << hero["stats"]["vigor"] << endl;
+        cout << "Mobility : " << hero["stats"]["mobility"] << endl;
+        cout << "Charisma : " << hero["stats"]["charisma"] << endl;
+        cout << "Intellect: " << hero["stats"]["intellect"] << endl;
 
-    cout << "------------------------\n";
-        }
+        cout << "------------------------\n";
+    }
 }
 
 void menuAdmin() {
     int pilihan;
 
+
     do {
         cout << "\n===== MENU ADMIN =====\n";
-        cout << "1. Lihat Data Hero\n";
-        cout << "2. Info Sistem\n";
-        cout << "0. Logout\n";
+        cout << "1. Pengelolaan Superhero\n";
+        cout << "2. Pengelolaan Dispatcher\n";
+        cout << "0. Keluar\n";
         cout << "Pilih: ";
         cin >> pilihan;
 
-        switch (pilihan) {
-            case 1:
-                tampilkanHero();  
-                break;
-
-            case 2:
-                cout << "Sistem Superhero Dispatch v1.0\n";
-                break;
-
-            case 0:
-                cout << "Logout admin...\n";
-                break;
-
-            default:
-                cout << "Pilihan tidak valid\n";
+        if (pilihan == 1) {
+            tampilkanHero();
+        }
+        else if (pilihan == 2) {
+            cout << "\n=== PENGELOLAAN DISPATCHER ===\n";
+            cout << "Fitur masih dalam pengembangan...\n";
+        }
+        else if (pilihan == 0) {
+            cout << "Anda akan keluar dari menu ini...\n";
+        }
+        else {
+            cout << "Pilihan tidak valid!\n";
         }
 
     } while (pilihan != 0);
